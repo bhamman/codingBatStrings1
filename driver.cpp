2f87a87c70ba8677bf9cf19db9efd39a94906137
bhamman <rhamman@stu.ctcd.edu>
2017-04-24 17:11:00 -0500

    completed string04

--- driver.cpp
+++ driver.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <cassert>
 using namespace std;
 
-
+string makeOutWord(string out, string word);
 
 int main()
 {
-    cout<< "Hello world!" << endl;
     
+    assert(makeOutWord("<<>>", "Yay") == "<<Yay>>");
+    assert(makeOutWord("<<>>", "WooHoo") == "<<WooHoo>>");
+    assert(makeOutWord("[[]]", "word") == "[[word]]");
+    assert(makeOutWord("{{}}", "Hello") == "{{Hello}}");
+    assert(makeOutWord("(())", "BOOM") == "((BOOM))");
 
 
     return 0;
+}
+
+string makeOutWord(string out, string word)
+{
+      
+    return out.insert(2, word);
+
 }
